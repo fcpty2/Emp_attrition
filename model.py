108cd832cf2bf435d563e0a082791aa98c918c30
fcpty2 <p@Pattas-MacBook-Pro.local>
2021-06-23 22:05:55 +0700

    create model

---
+++ model.py
@@ -0,0 +1,11 @@
+# Import libraries
+import pandas as pd
+import numpy as np
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+
